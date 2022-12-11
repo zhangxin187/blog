6ce24e08e0197310a2d198faa1d72a9e25f2f574
zhangxin <862257810@qq.com.com>
2022-12-11 21:31:24 +0800

    deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 export default {
   title: "个人笔记",
-  description: "12312312",
+  base: '/blog/',
   themeConfig: {
     socialLinks: [
       { icon: "github", link: "https://github.com/zhangxin187" },
@@ -12,21 +12,21 @@ export default {
         text: "React",
         collapsible: true,
         items: [
-          { text: "React知识点", link: "/blog/react-learning" },
+          { text: "React知识点", link: "/react-learning" },
         ],
       },
       {
         text: "Vue",
         collapsible: true,
         items: [
-          { text: "Vue知识点", link: "/blog/vue-learning" },
+          { text: "Vue知识点", link: "/vue-learning" },
         ],
       },
       {
         text: "面试问题总结",
         collapsible: true,
         items: [
-          { text: "前端", link: "/blog/other-learning" },
+          { text: "前端", link: "/other-learning" },
         ],
        
        
